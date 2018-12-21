50a6b73496c0c60607c8532a98b6665b1ae6b06a
DopaA <2534423312@qq.com>
2018-12-21 14:52:21 +0800

    后端数据库管理
    主要是后端数据的强制删除等

---
+++ userManager/userManager/user.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace userManager
+{
+    public class user
+    {
+        [Key]
+        public string Wxid { set; get; }
+        public int MsId { set; get; }
+        public string Name { set; get; }
+        public string Content { set; get; }
+        public string Time { set; get; }
+        public string Region { set; get; }
+        public string Imageid { set; get; }
+        public user() { }
+        public user(int msId, string name, string wxid, string content, string time, string region, string imageid)
+        {
+            this.MsId = msId;
+            this.Name = name;
+            this.Wxid = wxid;
+            this.Content = content;
+            this.Time = time;
+            this.Region = region;
+            this.Imageid = imageid;
+        }
+        public override string ToString()
+        {
+            return MsId + Name + Wxid + Content + Time + Region + Imageid;
+        }
+    }
+}
